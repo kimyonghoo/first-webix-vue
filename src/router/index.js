9ec484e50787c42bd52c4ac5f8c6cf98c82730da
김용후 <2013502@clt.net>
2019-08-28 14:07:00 +0900

    change routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,12 @@ import Main from '../component/Main.vue'
 import StockManagement from '../component/screen/ManageProductStock.vue'
 import SellerManagement from '../component/screen/ManageSeller.vue'
 
+const Home = {template: '<div><h2>HOME</h2></div>'}
 Vue.use(VueRouter);
 const router = new VueRouter({
     mode: 'history',
     routes: [
-      { path: '/', component: Main},
+      { path: '/', component: Home},
       { path: '/Main', component: Main},
       { path: '/stockmanagement/:category', name: 'stockmanagement', component: StockManagement},
       { path: '/sellermanagement', component: SellerManagement}
